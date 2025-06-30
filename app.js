f26230ee07ec489dc1106749028c5035e162fa99
cristina94m@gmail.com <cristina94m@gmail.com>
2025-06-30 16:33:32 +0200

    added CORS config

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
 
 // middleware para JSON
+app.use(
+  cors({
+    origin: "http://localhost:3001",
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 
 // rutas de users
@@ -14,6 +23,5 @@ app.use("/games", gamesRoutes);
 app.use((req, res) => {
   res.status(404).json({ error: "Route not found" });
 });
-
 
 module.exports = app;
